308434007c9ce53ae14589a80c034ebcb0dbebc0
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2025-09-13 19:59:46 +0530

    added config

--- src/lib/firebase.jsx
+++ src/lib/firebase.jsx
@@ -2,13 +2,22 @@ import Firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
-// import the seed file heare
+// import the seed file here
+import { seedDatabase } from '../seed';
 
-const config = {};
+const config = {
+  apiKey: 'AIzaSyBg0q1uVXK58qsJSMIgo5Dq7XmYQ108YiI',
+  authDomain: 'hrk-mern-blog.firebaseapp.com',
+  projectId: 'hrk-mern-blog',
+  storageBucket: 'hrk-mern-blog.firebasestorage.app',
+  messagingSenderId: '127506150714',
+  appId: '1:127506150714:web:69b629e94ec03098578c03',
+};
 
 const firebase = Firebase.initializeApp(config);
 const { fieldValue } = Firebase.firestore;
 
 // call the seed file only onece
+seedDatabase(firebase);
 
 export { firebase, fieldValue };
